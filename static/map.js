5cacb061d4d4a0b1eb44c0ba1146da4bbfd1c062
uncamy@gmail.com <uncamy@gmail.com>
2013-11-26 17:33:59 -0500

    need to add heat map data

--- static/map.js
+++ static/map.js
@@ -1,8 +1,16 @@
 /*javascript needed for google maps rendering */
 
 /*var latlng= new google.maps.LatLng(document.getElementById("user_loc").innerHTML);*/
+var map, pointarray, heatmap;
+/*
+var gradeData = [
+    new google.maps.LatLng(man_grades.geocodes)]; */
 
-var latlng = new google.maps.LatLng(40.72078, -74.001119);
+console.log(man_grades.geocodes);
+
+
+
+//var latlng = new google.maps.LatLng(40.72078, -74.001119);
 /*graffiti_locations passed in as graffiti_loc*/
 
 function initialize(){
@@ -20,7 +28,6 @@ function initialize(){
         title: "user location",
         map: map
     });
-
     /*add marker for graffiti locations */
     var image = {
         url:'http://www.alispagnola.com/Free/spraypaint2.jpg',
@@ -29,16 +36,7 @@ function initialize(){
     };
 
 
-    for(var i = 0; i<graffiti_loc.length; i++){
-        var graffiti = graffiti_loc[i];
-        var location = new google.maps.LatLng(graffiti[0], graffiti[1]);
-        var graffiti_marker = new google.maps.Marker({
-        icon: image,
-        position: location,
-        title: "graffiti",
-        map: map
-    });
 }
 
-}
+
 google.maps.event.addDomListener(window, 'load', initialize);
